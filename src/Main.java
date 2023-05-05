31663d2e561d4390d8c367d4ab308a5fb11712ad
Harsh <harshmehra53@gmail.com>
2023-05-05 13:07:48 +0530

    Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 24;
+        int c = 36;
+
+        int sum = a + b + c;
+
+        System.out.println(sum);
+
+    }
+}
