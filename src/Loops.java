f9dc96e4d6a3b1436e153c6c38e535a35409c818
Harsh <harshmehra53@gmail.com>
2023-05-13 13:39:57 +0530

    up

--- src/Loops.java
+++ src/Loops.java
@@ -21,29 +21,37 @@ public class Loops {
 
     /* 2. Printing an table */
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
+// public class Loops{
+//         public static void main(String[] args) {
+//             Scanner s = new Scanner(System.in);
+//             int num;
+//             int range;
+//             int i = 1;
+
+//             System.out.print("Enter the value of num: ");
+//             num = s.nextInt();
+//             System.out.print("Enter the range of table: ");
+//             range = s.nextInt();
+
+//             while(i<=range){
+//                 System.out.println(num + " * " + i + " = " + num * i);
+//                 i++;
+//             }
+
+//         }
+//     }
+
+
+/* 3. Print first 5 odd numbers  */
 public class Loops{
-        public static void main(String[] args) {
-            Scanner s = new Scanner(System.in);
-            int num;
-            int range;
-            int i = 1;
-
-            System.out.print("Enter the value of num: ");
-            num = s.nextInt();
-            System.out.print("Enter the range of table: ");
-            range = s.nextInt();
-
-            while(i<=range){
-                System.out.println(num + " * " + i + " = " + num * i);
-                i++;
-            }
-
+    public static void main(String[] args) {
+        for(int i = 1; i<=10 ; i=i+2){
+            System.out.println(i);
         }
     }
-
-
+}
 
 
 
